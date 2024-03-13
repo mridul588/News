ca48a842fd57c8d6c11659196ed3d7b054d9f70a
mridul589 <tmridul79@gmail.com>
2024-03-13 22:24:31 +0530

    production code

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use("/api",newsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // Establishes the path to our frontend (most important)
-  app.use(express.static(path.join(DIRNAME, "/client/dist")));
+  app.use(express.static(path.join(DIRNAME, "/Client/dist")));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(DIRNAME, "/client/dist/index.html"))
+    res.sendFile(path.join(DIRNAME, "/Client/dist/index.html"))
   );
 }
 
